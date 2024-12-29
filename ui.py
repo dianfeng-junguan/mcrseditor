da484aa0324a427445504e74904ff3b226023773
xexux <1770711918@qq.com>
2024-12-29 21:13:48 +0800

    实现了ui界面添加连线和门电路的功能

--- ui.py
+++ ui.py
@@ -6,110 +6,11 @@ import nbtrd
 import python_nbt.nbt as nbt
 import tkinter as tk
 import os
+from tkinter import filedialog
 from queue import Queue
 
 comm=Queue(32)#两个线程通信用 cmd2display
 com_dis2cmd=Queue(32)#display2cmd
-def display_thread():
-    selmode=''
-    selgate=''
-    # pygame.init()  #内部各功能模块进行初始化创建及变量设置，默认调用
-    # pygame.display.set_caption("MCrs")  #设置显示窗口的标题内容，是一个字符串类型
-    size = width,height = 800,600  #设置游戏窗口大小，分别是宽度和高度
-    BLOCK_RENDERW=20
-    #tkinter内嵌pygame 以便于加gui
-    tkroot=tk.Tk()
-    frame=tk.Frame(tkroot,width=width,height=height)
-    frame.pack()
-    os.environ['SDL_WINDOWID'] = str(frame.winfo_id())
-    os.environ['SDL_VIDEODRIVER'] = 'windib'
-    tkroot.update()
-    #菜单栏# 创建顶层菜单
-    menubar = tk.Menu(tkroot)
-
-    # 添加菜单项
-    
-    filemenu=tk.Menu(menubar)
-    filemenu.add_command(label='Open')
-    filemenu.add_command(label='Save')
-    filemenu.add_command(label='New')
-
-    editmenu=tk.Menu(menubar)
-    gatemenu=tk.Menu(editmenu)
-    gatemenu.add_command(label='And',accelerator='shift+a',command=lambda :put_gate('and'))
-    gatemenu.add_command(label='Or',accelerator='shift+o' ,command=lambda :put_gate('or'))
-    gatemenu.add_command(label='Not',accelerator='shift+n',command=lambda :put_gate('not'))
-
-    editmenu.add_cascade(label='Add Gate',menu=gatemenu)
-    # 将下拉菜单添加到顶层菜单项
-    menubar.add_cascade(label='Files', menu=filemenu)
-    menubar.add_cascade(label='Edit', menu=editmenu)
-
-    # 显示菜单
-    tkroot.config(menu=menubar)
-
-    pygame.display.init()
-    pygame.font.init()
-    screen = pygame.display.set_mode(size)  #初始化显示窗口
-    
-    def draw_gate(gate,rect):
-        pygame.draw.rect(screen,(100,100,100),(rect[0],rect[1],rect[0]+rect[2],rect[1]+rect[3]),\
-                         width=3)
-        font=pygame.font.SysFont('arial',15)
-        screen.blit(font.render(gate,True,(255,255,255)),vadd(rect,(10,10)))
-    def deal_sel(curpos:tuple[int,int]):
-        nonlocal selgate,selmode
-        '''
-        处理鼠标模式（放置门etc)
-        '''
-        blkpos=(int(curpos[0]/BLOCK_RENDERW),int(curpos[1]/BLOCK_RENDERW))
-        if selmode=='gate':
-            com_dis2cmd.put('gate '+selgate+" %d,%d"%(blkpos[0],blkpos[1]))
-        #清空状态
-        selmode=''
-    
-    #鼠标模式:=gate为放置门电路
-    def put_gate(type:str):
-        nonlocal selmode,selgate
-        '''
-        在界面中放置门
-        '''
-        selgate=type
-        selmode='gate'
-    while True:  #无限循环，直到Python运行时退出结束
-        
-        if not comm.empty():
-            #有来自cmd的消息
-            data:list=comm.get()
-            #解析
-            if data[0]=='setblock':
-                rect=[e*BLOCK_RENDERW for e in data[1]]
-                pygame.draw.rect(screen,(255,255,255),(rect[0],rect[1],rect[0]+BLOCK_RENDERW,rect[1]+BLOCK_RENDERW))
-            elif data[0]=='q':
-                return
-            elif data[0]=='update':
-                pygame.display.update()
-            elif data[0]=='gate':
-                # rect=[e*BLOCK_RENDERW for e in data[2]]
-                # pygame.draw.rect(screen,(100,100,100),(rect[0],rect[1],rect[0]+rect[2],rect[1]+rect[3]),\
-                #                  width=3)
-                # font=pygame.font.SysFont('arial',15)
-                # screen.blit(font.render(data[1],True,(255,255,255)),vadd(rect,(10,10)))
-                draw_gate(data[1],rect)
-                #TODO 绘制端口位置
-
-
-        for event in pygame.event.get():  #从Pygame的事件队列中取出事件，并从队列中删除该事件
-            if event.type == pygame.QUIT:  #获得事件类型，并逐类响应
-                return
-            elif event.type==pygame.MOUSEMOTION:
-                curpos=pygame.mouse.get_pos()
-                if selmode=='gate':
-                    draw_gate(selgate,curpos+tuple(gates[selgate]['size']))
-            elif event.type==pygame.MOUSEBUTTONDOWN:
-                deal_sel(pygame.mouse.get_pos())
-        pygame.display.update()  #对显示窗口进行更新，默认窗口全部重绘
-        tkroot.update()
 
 
 helper="help\t\
@@ -142,6 +43,16 @@ def available(pos,size)->bool:
 blkmap=[]
 conn=[]
 curf=''
+def openf(given:str):
+    global curf,blkmap,conn
+    with open(given,'r') as f:
+            d=json.load(f)
+            blkmap=d['components']
+            conn=d['connections']
+    curf=given
+def savef(path:str):
+    with open(path,'w') as f:
+        json.dump({'components':blkmap,'connections':conn},f)
 def solve(cmd:str):
     global blkmap,conn,curf
     args=cmd.strip().lower().split(' ')
@@ -161,14 +72,9 @@ def solve(cmd:str):
     elif args[0]=='save':
         if len(args)<2:
             args.append(curf)
-        with open(args[1],'w') as f:
-            json.dump({'components':blkmap,'connections':conn},f)
+        savef(args[1])
     elif args[0]=='open':
-        with open(args[1],'r') as f:
-            d=json.load(f)
-            blkmap=d['components']
-            conn=d['connections']
-        curf=args[1]
+        openf(args[1])
         for gt in blkmap:
             for x in range(gt['rect'][2]):
                 for y in range(gt['rect'][3]):
@@ -258,12 +164,171 @@ if len(sys.argv)>1:
     comm.put(['q'])
     sys.exit()
 
+def draw_gate(gate,rect):
+    pygame.draw.rect(buffer,(100,100,100),(rect[0],rect[1],rect[2],rect[3]),\
+                    width=3)
+    font=pygame.font.SysFont('arial',15)
+    buffer.blit(font.render(gate,True,(255,255,255)),vadd(rect,(10,10)))
+def deal_sel(curpos:tuple[int,int]):
+    '''
+    处理鼠标模式（放置门etc)
+    '''
+    global selgate,selmode
+    blkpos=[e-e%BLOCK_RENDERW for e in curpos]
+    if selmode=='gate':
+        gt=gates[selgate]
+        if not available(blkpos,gt['size']):
+            print('failed placing gate: place not enough')
+            return
+        blkmap.append({"type":selgate,"rect":blkpos+(gt['size'])})
+        #清空状态
+        selmode=''
+    elif selmode=='line1':
+        global linep1
+        linep1=blkpos
+        selmode='line2'
+    elif selmode=='line2':
+        global linep2,linedir
+        #做出选择:h or v
+        if linedir=='h':
+            linep2=[blkpos[0],linep1[1]]
+        else:
+            linep2=[linep1[0],blkpos[1]]
+        
+        addline([int(e/BLOCK_RENDERW) for e in linep1],[int(e/BLOCK_RENDERW) for e in linep2])
+        selmode=''
+def addline(p1,p2):
+    '''
+    p1,p2应该是以方块为单位的坐标
+    '''
+    l=p1+[p2[0]-p1[0],p2[1]-p1[1]]
+    conn.append(l)
+#鼠标模式:=gate为放置门电路
+def put_gate(type:str):
+    global selgate,selmode
+    '''
+    在界面中放置门
+    '''
+    selgate=type
+    selmode='gate'
+def put_line():
+    global selmode
+    selmode='line1'
+def menuopen():
+    path=filedialog.askopenfilename()
+    openf(path)
+def menusave():
+    global curf
+    if len(curf)==0:
+        curf=filedialog.asksaveasfilename()
+    savef(curf)
+def menunew():
+    global curf,selmode
+    curf=''
+    blkmap.clear()
+    conn.clear()
+    selmode=''
 if __name__=='__main__':
-    #设置pygame线程
-    displayth=threading.Thread(target=display_thread,daemon=True)
-    displayth.start()
-    while True:
-        # cmd=input('cmd:')
-        if not com_dis2cmd.empty():
-            solve(com_dis2cmd.get())
+    selmode=''
+    selgate=''
+    linep1,linep2=[0,0],[0,0]
+    linedir='h'
+    # pygame.init()  #内部各功能模块进行初始化创建及变量设置，默认调用
+    # pygame.display.set_caption("MCrs")  #设置显示窗口的标题内容，是一个字符串类型
+    size = width,height = 800,600  #设置游戏窗口大小，分别是宽度和高度
+    BLOCK_RENDERW=20
+    #tkinter内嵌pygame 以便于加gui
+    tkroot=tk.Tk()
+    frame=tk.Frame(tkroot,width=width,height=height)
+    frame.pack()
+    os.environ['SDL_WINDOWID'] = str(frame.winfo_id())
+    os.environ['SDL_VIDEODRIVER'] = 'windib'
+    tkroot.update()
+    #菜单栏# 创建顶层菜单
+    menubar = tk.Menu(tkroot)
+    # 添加菜单项
+
+    filemenu=tk.Menu(menubar)
+    filemenu.add_command(label='Open',command=menuopen)
+    filemenu.add_command(label='Save',command=menusave)
+    filemenu.add_command(label='New',command=menunew)
+    editmenu=tk.Menu(menubar)
+    gatemenu=tk.Menu(editmenu)
+    gatemenu.add_command(label='And',accelerator='Ctrl+A',command=lambda :put_gate('and'))
+    gatemenu.bind_all("<Control-a>",lambda arg:put_gate('and'))
+    gatemenu.add_command(label='Or',accelerator= 'Ctrl+O' ,command=lambda :put_gate('or'))
+    gatemenu.bind_all("<Control-o>",lambda arg:put_gate('or'))
+    gatemenu.add_command(label='Not',accelerator='Ctrl+N',command=lambda :put_gate('not'))
+    gatemenu.bind_all("<Control-n>",lambda arg:put_gate('not'))
+
+    editmenu.add_cascade(label='Add Gate',menu=gatemenu)
+    editmenu.add_command(label='Add Line',command=put_line)
+    # 将下拉菜单添加到顶层菜单项
+    menubar.add_cascade(label='Files', menu=filemenu)
+    menubar.add_cascade(label='Edit', menu=editmenu)
+    # 显示菜单
+    tkroot.config(menu=menubar)
+    pygame.display.init()
+    pygame.font.init()
+    screen = pygame.display.set_mode(size)  #初始化显示窗口
+    buffer=pygame.Surface(size)
+
+    while True:  #无限循环，直到Python运行时退出结束
+        buffer.fill((0,0,0))
+        if not comm.empty():
+            #有来自cmd的消息
+            data:list=comm.get()
+            #解析
+            if data[0]=='setblock':
+                rect=[e*BLOCK_RENDERW for e in data[1]]
+                pygame.draw.rect(screen,(255,255,255),(rect[0],rect[1],rect[0]+BLOCK_RENDERW,rect[1]+BLOCK_RENDERW))
+            elif data[0]=='q':
+                break
+            elif data[0]=='update':
+                pygame.display.update()
+            elif data[0]=='gate':
+                # rect=[e*BLOCK_RENDERW for e in data[2]]
+                # pygame.draw.rect(screen,(100,100,100),(rect[0],rect[1],rect[0]+rect[2],rect[1]+rect[3]),\
+                #                  width=3)
+                # font=pygame.font.SysFont('arial',15)
+                # screen.blit(font.render(data[1],True,(255,255,255)),vadd(rect,(10,10)))
+                draw_gate(data[1],rect)
+                #TODO 绘制端口位置
+        #绘制已经放置的
+        for e in blkmap:
+            draw_gate(e['type'],[ee*BLOCK_RENDERW for ee in e['rect']])
+        for e in conn:
+            conrect=[max(ee*BLOCK_RENDERW,BLOCK_RENDERW) for ee in e]
+            pygame.draw.rect(buffer,(255,255,255),conrect)
+        curpos=pygame.mouse.get_pos()
+        
+        for event in pygame.event.get():  #从Pygame的事件队列中取出事件，并从队列中删除该事件
+            if event.type == pygame.QUIT:  #获得事件类型，并逐类响应
+                break
+            elif event.type==pygame.MOUSEMOTION:
+                if selmode=='line2':
+                    #确定方向
+                    delta=[curpos[0]-linep1[0],curpos[1]-linep1[1]]
+                    if abs(delta[0])>abs(delta[1]):
+                        linedir='h'
+                    else:
+                        linedir='v'
+                    linep2=[e-e%BLOCK_RENDERW for e in curpos]
+            elif event.type==pygame.MOUSEBUTTONDOWN:
+                deal_sel(pygame.mouse.get_pos())
+        #绘制鼠标上面的内容
+        if selmode=='gate':
+            drawpos=[e-e%BLOCK_RENDERW for e in curpos]
+            drawpos+=[e*BLOCK_RENDERW for e in gates[selgate]['size']]
+            draw_gate(selgate,drawpos)
+        elif selmode == 'line1':
+            pygame.draw.rect(buffer,(255,255,255),[e-e%BLOCK_RENDERW for e in curpos]+[BLOCK_RENDERW,BLOCK_RENDERW])
+        elif selmode=='line2':
+            i=0 if linedir=='h' else 1
+            length=linep2[i]-linep1[i]
+            pygame.draw.rect(buffer,(255,255,255),linep1+[length if not i else BLOCK_RENDERW,length if i else BLOCK_RENDERW])
+                
+        screen.blit(buffer,(0,0))
+        pygame.display.flip()  #对显示窗口进行更新，默认窗口全部重绘
+        tkroot.update()
 
